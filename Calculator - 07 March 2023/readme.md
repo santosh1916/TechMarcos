141be4ee88911626139667bd8edeaadce9a481ed
santosh1916 <santoshsahni1916@gmail.com>
2024-03-07 16:37:15 +0530

    Calculater task1 07 March Given by Avtar sir

---
+++ Calculator - 07 March 2023/readme.md
@@ -0,0 +1,8 @@
+1. Update calculator code. You must use below code
+<input type="text" id="num1">
+<input type="text" id="num2">
+<button onclick="add()">Add</button>
+<button onclick="subtract()">Subtract</button>
+<button onclick="multiply()">Multiply</button>
+<button onclick="divide()">Divide</button>
+<p id="result"></p>
--- Calculator/script.js
+++ Calculator/script.js
@@ -6,11 +6,10 @@ function appendToDisplay(value) {
         function clearDisplay() {
             document.getElementById('display').value = '';
         }
-
         function calculate() {
-            try {
-                document.getElementById('display').value = eval(document.getElementById('display').value);
-            } catch (error) {
-                document.getElementById('display').value = 'Error';
-            }
-        }
+        try {
+            document.getElementById('display').value = eval(document.getElementById('display').value);
+        } catch (error) {
+        document.getElementById('display').value = 'Error';
+    }
+}
