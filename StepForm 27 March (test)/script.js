a4cc5b2e8e6a09e076c5a8fa58b91dd287f1b77b
santosh1916 <santoshsahni1916@gmail.com>
2024-03-28 10:02:06 +0530

    update file

---
+++ StepForm 27 March (test)/script.js
@@ -0,0 +1,23 @@
+document.addEventListener('DOMContentLoaded', function() {
+   var firstNameInput = document.getElementById('firstname');
+   var lastNameInput = document.getElementById('lastname');
+   var addressInput = document.getElementById('address');
+   var phoneInput = document.getElementById('phone');
+   var fullNamePreview = document.getElementById('fullname');
+   var addressPreview = document.getElementById('addressline');
+   var phonePreview = document.getElementById('phonenumber');
+
+
+   // event when input filled
+   firstNameInput.addEventListener('input', updatePreview);
+   lastNameInput.addEventListener('input', updatePreview);
+   addressInput.addEventListener('input', updatePreview);
+   phoneInput.addEventListener('input', updatePreview);
+
+
+   function updatePreview() {
+
+       let fullName = firstNameInput.value + ' ' + lastNameInput.value;
+       fullNamePreview.textContent = fullName;
+   }
+});
