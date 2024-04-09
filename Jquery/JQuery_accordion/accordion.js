ac20e78397a63f170dbe2d25f6f29fff202acc25
santosh1916 <santoshsahni1916@gmail.com>
2024-04-09 12:27:00 +0530

    drag-and-drop interface for images using jQuery UI's draggable and droppable features. Need uploaded image preview also

--- Jquery/JQuery_accordion/accordion.js
+++ Jquery/JQuery_accordion/accordion.js
@@ -2,3 +2,80 @@ $("#accordion").accordion({
     collapsible: true,
     heightStyle: "fill"
 })
+
+$( function() {
+    $( ".row" ).sortable({
+        items: $(".gallery-img")
+    });
+  });
+
+
+  $(".btn[title='PHILOSOPHY']").click(function() {
+    $(this).next(".subnav").toggleClass("scale-up" ,  "scale-down");
+});
+
+
+
+
+
+//   Handle add image from
+let ImageGalleryDatabase = JSON.parse(localStorage.getItem("Gallery")) || [];
+
+$(document).ready(function() {
+  $('#chooseImage').on('drop', function(event) {
+    event.preventDefault();
+    handleFiles(event.originalEvent.dataTransfer.files);
+  });
+
+  $('#chooseImage').on('change', function() {
+    handleFiles($('#chooseImage')[0].files);
+  });
+});
+
+// Function to handle selected files
+function handleFiles(files) {
+  if (files.length > 0) {
+    let fragment = document.createDocumentFragment();
+    for (let i = 0; i < files.length; i++) {
+      let reader = new FileReader();
+      let inputFileName = files[i].name;
+
+      reader.onload = function() {
+        let fileData = {
+          name: inputFileName,
+          dataURL: reader.result
+        };
+        ImageGalleryDatabase.push(fileData);
+        localStorage.setItem("Gallery", JSON.stringify(ImageGalleryDatabase));
+        if (i === files.length - 1) {
+          $('#chooseImage').val(""); // Reset file input field
+          alert("Data saved to localStorage. Please reload the page.");
+        }
+      };
+      reader.readAsDataURL(files[i]);
+    }
+  }
+}
+
+// Show all cards in the gallery
+function showData() {
+  let html = "";
+  ImageGalleryDatabase.forEach((img, index) => {
+    html += createCard(img, index);
+  });
+  $("#gallery-container").prepend(html);
+}
+
+// Function to create a single card
+function createCard(img, index) {
+  return `<div class="column gallery-img">
+            <div class="card">
+              <div class="card-content"></div>
+              <img class="card-img" src="${img.dataURL}"> 
+            </div>
+          </div>`;
+}
+
+// Initial display of data
+showData();
+
